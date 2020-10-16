a6fe93d8d8f640271b104dd42268c2da1104510d
Kai Heinrich <kai.heinrich91@web.de>
2020-10-16 13:40:19 +0200

    Add Get and Post Requests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import AddToDo from "./components/AddToDo";
+import AddToDoForm from "./components/AddToDoForm";
 
 function App() {
-  return (
-    <div >
-      Hallo React start
+  const url = "/api/todo";
+  const [toDoList, setToDoList] = useState([]);
+
+
+  useEffect(() => {
+      axios.get(url)
+          .then(response => response.data)
+          .then(data => setToDoList(data))
+          .catch(error => console.log(error));
+  }, ([], [toDoList]));
+
+    return (
+    <div>
+        {toDoList.map(item => <div key = {item.id}> {item.description} {item.status}</div>)}
+        <div>
+          <AddToDoForm/>
+        </div>
     </div>
   );
 }
---
+++ frontend/src/components/AddToDoForm.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+export default function AddToDoForm() {
+    const [description, setDescription] = useState("");
+    const [status, setStatus] = useState("");
+    const selectValues = ["OPEN", "IN_PROGRESS", "DONE"];
+
+    return <form>
+        <label>Add new description
+        <input value={description} onChange={event => setDescription(event.target.value)}/>
+        </label>
+        <label>Add new status
+            <select onChange={event => setStatus(event.target.value)}>
+                <option value="OPEN">OPEN</option>
+                <option value="IN_PROGRESS">IN PROGRESS</option>
+                <option value="DONE">DONE</option></select>
+        </label>
+
+        <button disabled={description.length === 0 || status.length === 0} type="button" onClick={() => {
+            sendRequest({description, status})
+            setDescription("");
+            setStatus("");
+        }}>Enter</button>
+    </form>
+}
+
+function sendRequest({description, status}){
+    const url = "/api/todo"
+    const newToDo = {description, status}
+    axios.post(url, newToDo).catch(error => console.log(error));
+}
